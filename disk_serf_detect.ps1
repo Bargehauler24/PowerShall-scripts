14a0776c9cddbdbacf98fd03d7ff2270e93a7147
i.burlachenkov <i.burlachenkov@mail.ru>
2023-04-12 17:39:43 +0300

    smb_scripts

---
+++ disk_serf_detect.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "SilentlyContinue" #скрытие ошибок
+$pcs = Get-ADComputer -Filter "OperatingSystem -notlike '*Server*'" | Where-Object {($_.Enabled -eq $true) } #командлит на выборку всех компов
+foreach ($pc in $pcs){
+    if (Test-Connection -ComputerName $pc.Name -Quiet -Count 1){
+        $disk = Invoke-Command -ComputerName $pc.Name -ScriptBlock{Get-PSDrive -name R}
+        if($disk.Description -eq 'ЭЦП'){
+            Write-Host $pc.Name "есть диск " $disk.Name $disk.Description}
+       }
+}
---
+++ smb01_active(v2.0).ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = "SilentlyContinue" #скрытие ошибок
+$pcs = Get-ADComputer -Filter "OperatingSystem -notlike '*Server*'" | Where-Object {($_.Enabled -eq $true) } #командлит на выборку всех компов
+foreach ($pc in $pcs){
+    if (Test-Connection -ComputerName $pc.Name -Quiet -Count 1){
+        $status =Get-Service -ComputerName $pc.Name mrxsmb10
+        if($status.Status -eq 'Running'){
+            Write-Host $pc.Name "служба SMB1: " $status.Status
+        }else{
+            Write-Host $pc.Name "служба SMB1 не найдена"}
+       }
+}
