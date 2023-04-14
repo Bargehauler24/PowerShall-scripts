9af2992e42be462dcca0218e56b242fd1bc0a3ce
i.burlachenkov <i.burlachenkov@mail.ru>
2023-04-14 16:08:58 +0300

    radmin_script

---
+++ radmin_all_reboot_pass.ps1
@@ -0,0 +1,15 @@
+﻿#$z = Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Radmin\v3.0\Server\Parameters\Radmin Security\1"
+#Set-Content C:\admin.txt $z.1
+$reg =gc D:\test4.txt
+$ErrorActionPreference = "SilentlyContinue" #скрытие ошибок
+$pcs = Get-ADComputer -Filter "OperatingSystem -notlike '*Server*'" | Where-Object {($_.Enabled -eq $true) } #командлит на выборку всех компов
+foreach ($pc in $pcs){
+    if (Test-Connection -ComputerName $pc.Name -Quiet -Count 1){
+        Invoke-Command -ComputerName $pc.Name -ScriptBlock {Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Radmin\v3.0\Server\Parameters\Radmin Security\1" -Name 1}
+        Invoke-Command -ComputerName $pc.Name -ScriptBlock {New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Radmin\v3.0\Server\Parameters\Radmin Security\1" -Name 1 -PropertyType Binary -Value $using:reg }
+        Write-Host $pc.Name "смена пароля произошла успешна"
+    } else {
+        Write-Host $pc.Name "не подключился"
+    }
+
+}
---
+++ radmin_all_reboot_pass_array.ps1
@@ -0,0 +1,15 @@
+﻿$reg =gc D:\test4.txt
+$mass = gc D:\tr.txt
+$ErrorActionPreference = "SilentlyContinue" #скрытие ошибок
+$array = $mass
+#$pcs = Get-ADComputer -Filter "OperatingSystem -notlike '*Server*'" | Where-Object {($_.Enabled -eq $true) } #командлит на выборку всех компов
+foreach ($pc in $array){
+    if (Test-Connection -ComputerName $pc -Quiet -Count 1){
+        Invoke-Command -ComputerName $pc -ScriptBlock {Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Radmin\v3.0\Server\Parameters\Radmin Security\1" -Name 1}
+        Invoke-Command -ComputerName $pc -ScriptBlock {New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Radmin\v3.0\Server\Parameters\Radmin Security\1" -Name 1 -PropertyType Binary -Value $using:reg }
+        Write-Host $pc "смена пароля произошла успешна"
+    } else {
+        Write-Host $pc "не подключился"
+    }
+
+}
